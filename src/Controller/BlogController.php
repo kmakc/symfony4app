14656485fde3bbba2b9381eff62ae6821fa185f1
Maxim Kovalsky <maxim.kovalsky@alfaforex.com>
2018-07-22 15:34:58 +0300

    controllers, routers, views: basics

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,9 +5,10 @@ use App\Service\Greeting;
 use App\Service\VeryBadDesign;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class BlogController extends AbstractController
+class BlogController
 {
     /**
      * @var Greeting;
@@ -19,19 +20,26 @@ class BlogController extends AbstractController
      */
     private $badDesign;
 
-    public function __construct(Greeting $greeting, VeryBadDesign $badDesign)
+    /**
+     * @var \Twig_Environment
+     */
+    private $twig;
+
+    public function __construct(Greeting $greeting, \Twig_Environment $twig)
     {
         $this->greeting  = $greeting;
-        $this->badDesign = $badDesign;
+        $this->twig      = $twig;
     }
 
     /**
-     * @Route("/", name="blog_index")
+     * @Route("/{name}", name="blog_index")
      */
-    public function indexAction(Request $request)
+    public function index($name)
     {
-        return $this->render('base.html.twig', ['message' => $this->greeting->greet(
-            $request->get('name')
+        $html = $this->twig->render('base.html.twig', ['message' => $this->greeting->greet(
+            $name
         )]);
+
+        return new Response($html);
     }
 }
