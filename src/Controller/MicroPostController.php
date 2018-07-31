cf5697474fecc0fea7fc30cfd027c371f68d0ad4
Maxim Kovalsky <maxim.kovalsky@alfaforex.com>
2018-07-31 13:30:08 +0300

    doctrine: deleting an entity and flash messages

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
 
@@ -42,17 +43,24 @@ class MicroPostController
      */
     private $router;
 
+    /**
+     * @var FlashBagInterface
+     */
+    private $flashBag;
+
     public function __construct(
         \Twig_Environment $twig, MicroPostRepository $microPostRepository,
         FormFactoryInterface $formFactory,
         EntityManagerInterface $entityManager,
-        RouterInterface $router
+        RouterInterface $router,
+        FlashBagInterface $flashBag
     ) {
         $this->twig = $twig;
         $this->microPostRepository = $microPostRepository;
         $this->formFactory = $formFactory;
         $this->entityManager = $entityManager;
         $this->router = $router;
+        $this->flashBag = $flashBag;
     }
 
     /**
@@ -90,6 +98,19 @@ class MicroPostController
                 'form' => $form->createView(),
             ])
         );
+    }
+
+    /**
+     * @Route("/delete/{id}", name="micro_post_delete")
+     */
+    public function delete(MicroPost $microPost)
+    {
+        $this->entityManager->remove($microPost);
+        $this->entityManager->flush();
+
+        $this->flashBag->add('notice', 'Micro post was deleted');
+
+        return new RedirectResponse($this->router->generate('micro_post_index'));
     }
 
     /**
