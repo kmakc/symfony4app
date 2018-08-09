e37897146366343154f06c51476ea6f53cb55dc7
Maxim Kovalsky <kmakc@yandex.ru>
2018-08-09 12:43:48 +0300

    sessions: creating new user preferences after user registration

--- src/EventSubscriber/UserSubscriber.php
+++ src/EventSubscriber/UserSubscriber.php
@@ -2,8 +2,10 @@
 
 namespace App\EventSubscriber;
 
+use App\Entity\UserPreferences;
 use App\Event\UserRegisterEvent;
 use App\Mailer\Mailer;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class UserSubscriber implements EventSubscriberInterface
@@ -13,9 +15,24 @@ class UserSubscriber implements EventSubscriberInterface
      */
     private $mailer;
 
-    public function __construct(Mailer $mailer)
+    /**
+     * @var string
+     */
+    private $defaultLocale;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(
+        Mailer $mailer,
+        EntityManagerInterface $entityManager,
+        string $defaultLocale)
     {
-        $this->mailer = $mailer;
+        $this->mailer        = $mailer;
+        $this->defaultLocale = $defaultLocale;
+        $this->entityManager = $entityManager;
     }
 
     public static function getSubscribedEvents()
@@ -27,6 +44,14 @@ class UserSubscriber implements EventSubscriberInterface
 
     public function onUserRegister(UserRegisterEvent $event)
     {
+        $preferences = new UserPreferences();
+        $preferences->setLocale($this->defaultLocale);
+
+        $user = $event->getRegisteredUser();
+        $user->setPreferences($preferences); // no need to persist, because cascade = persist
+
+        $this->entityManager->flush();
+
         $this->mailer->sendConfirmationEmail($event->getRegisteredUser());
     }
 }
